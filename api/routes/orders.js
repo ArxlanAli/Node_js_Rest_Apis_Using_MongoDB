ea3a5e7d6ee6f4fdfc135aae80be9eb74d8f5053
Arslan Ali <arslan29.may@gmail.com>
2018-07-08 20:19:54 +0500

    Second Commit

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Order = require('../models/orders')
+const Product = require('../models/products')
 
 const router = express.Router()
 
@@ -26,20 +27,31 @@ router.get('/',(req,res,err)=>{
 })
 
 router.post('/',(req,res,err)=>{
-    const order = new Order({
-        _id:mongoose.Types.ObjectId(),
-        quantity:req.body.quantity,
-        product:req.body.productId
-    })
-    order
-    .save()
-    .then(result=>{
-        console.log(result)
-        res.json({
-            order:result
+    Product.findById(req.body.productId).then(product=>{
+        if(!product){
+            return res.status(404).json({
+                message:"Product for this order is not found"
+            })
+        }
+        const order = new Order({
+            _id:mongoose.Types.ObjectId(),
+            quantity:req.body.quantity,
+            product:req.body.productId
         })
-    })
-    .catch(err=>{
+        order
+        .save()
+        .then(result=>{
+            console.log(result)
+            res.json({
+                order:result
+            })
+        })
+        .catch(err=>{
+            res.json({
+                error:err
+            })
+        })
+    }).catch(err=>{
         res.json({
             error:err
         })
@@ -47,13 +59,31 @@ router.post('/',(req,res,err)=>{
 })
 
 router.get('/:orderId',(req,res,err)=>{
-    const id = req.params.orderId
-    res.status(200).json({
-        message:id
+    Order.findById(req.params.orderId).exec().then(order=>{
+        res.status(200).json({
+            order:order
+        })
+    }).catch(error=>{
+        res.status(500).json({
+            error:error
+        })
     })
 })
 
+router.delete('/:orderId',(req,res,err)=>{
+    const orderId = req.params.orderId
+    Order.remove({_id : orderId}).exec().then(result=>{
+        res.status(200).json({
+            message:"order deleted"
+        })
+    }).catch(error=>{
+        res.status(500).json({
+            error:error
+        })
+    })
 
+
+})
 
 
 module.exports = router;
